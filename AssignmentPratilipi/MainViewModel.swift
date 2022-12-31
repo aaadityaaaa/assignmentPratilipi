5063901483f0103db9454f3db28d9288fe111a94
aaadityaaaa <aaadityaaazz@gmail.com>
2022-12-31 11:55:52 +0530

    initial setup

---
+++ AssignmentPratilipi/MainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewModel.swift
+//  AssignmentPratilipi
+//
+//  Created by Aaditya Singh on 30/12/22.
+//
+
+import Foundation
+
+
+class MainViewModel: ObservableObject {
+    //hehehe
+}
---
+++ AssignmentPratilipi/NewsVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewsVC.swift
+//  AssignmentPratilipi
+//
+//  Created by Aaditya Singh on 30/12/22.
+//
+
+import UIKit
+
+class NewsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray5
+        navigationController?.navigationBar.prefersLargeTitles = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
