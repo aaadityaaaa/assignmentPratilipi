08ae12356673fc448c07d9841c51f91bb80f984e
aaadityaaaa <aaadityaaazz@gmail.com>
2023-01-01 22:46:04 +0530

    changed the architecture from MVC

--- AssignmentPratilipi/ViewModel/MainViewModel.swift
+++ AssignmentPratilipi/ViewModel/MainViewModel.swift
@@ -8,36 +8,57 @@
 import Foundation
 import UIKit
 
+enum AppSection {
+    case topSection
+    case bottomSection
+}
 
-class MainViewModel: ObservableObject {
+struct MainViewModel {
+ 
+    func configureCollectionView(collectionView: UICollectionView, navigationController: UINavigationController?) {
+        collectionView.register(CompositionalHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "headerID")
+        collectionView.register(topSectionCell.self, forCellWithReuseIdentifier: "cellID")
+        collectionView.register(bottomSectionCell.self, forCellWithReuseIdentifier: "bottomCellID")
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
     
-//    @Published var isLoadingMoreObjects = false
-//    @Published var objects: [Object] = []
-//    @Published var hasMoreObjects = true
-//    var dataSource: UICollectionViewDiffableDataSource<Section, Object>!
-//    enum Section { case main }
-//
-//
-//    func updateUI(with: [Object]) {
-//        if objects.count < 10 { self.hasMoreObjects = false }
-//        self.objects.append(contentsOf: objects)
-//
-//        if self.objects.isEmpty {
-//           print("returned objects are empty")
-//            return
-//        }
-//
-//        self.updateData(on: self.objects)
-//    }
-//
-//
-//    func updateData(on objects: [Object]) {
-//        var snapshot = NSDiffableDataSourceSnapshot<Section, Object>()
-//        snapshot.appendSections([.main])
-//        snapshot.appendItems(objects)
-//        DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
-//    }
-//    
+    func configureLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionNumber, _) -> NSCollectionLayoutSection? in
+            if sectionNumber == 0 {
+               return MainViewModel.topSection()
+            }
+            else {
+               return MainViewModel.bottomSection()
+            }
+        }
+        return layout
+    }
     
+    static func topSection() -> NSCollectionLayoutSection? {
+        let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1/5), heightDimension: .fractionalHeight(0.9)))
+        item.contentInsets.top = 16
+        item.contentInsets.bottom = 16
+        item.contentInsets.trailing = 0
+        item.contentInsets.leading = 10
+
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(2), heightDimension: .absolute(320)), subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        return section
+    }
+    
+    static func bottomSection() -> NSCollectionLayoutSection? {
+        let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .absolute(295), heightDimension: .absolute(120)))
+        item.contentInsets = .init(top: 0, leading: 0, bottom: 16, trailing: 10)
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .absolute(305), heightDimension: .absolute(300)), subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets.leading = 16
+        let kind = UICollectionView.elementKindSectionHeader
+        section.boundarySupplementaryItems = [.init(layoutSize: .init(widthDimension : .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: kind, alignment: .topLeading)]
+        return section
+    }
     
 }
