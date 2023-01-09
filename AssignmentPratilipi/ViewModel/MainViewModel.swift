47157f1f503947785065219e59f4cb884a95fdab
aaadityaaaa <aaadityaaazz@gmail.com>
2023-01-09 18:03:45 +0530

    no big changes

--- AssignmentPratilipi/ViewModel/MainViewModel.swift
+++ AssignmentPratilipi/ViewModel/MainViewModel.swift
@@ -34,7 +34,7 @@ struct MainViewModel {
         }
         return layout
     }
-    
+    //MARK:- top section
     static func topSection() -> NSCollectionLayoutSection? {
         let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1/5), heightDimension: .fractionalHeight(0.9)))
         item.contentInsets.top = 16
@@ -51,8 +51,20 @@ struct MainViewModel {
     static func bottomSection() -> NSCollectionLayoutSection? {
         let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .absolute(295), heightDimension: .absolute(120)))
         item.contentInsets = .init(top: 0, leading: 0, bottom: 16, trailing: 10)
-        
         let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .absolute(305), heightDimension: .absolute(300)), subitems: [item])
+        
+//MARK:- custom group method example
+        
+//        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(300))
+//        let group = NSCollectionLayoutGroup.custom(layoutSize: groupSize, itemProvider: { (index: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutItem in
+//                return item
+//            })
+        
+//        let group2 = NSCollectionLayoutGroup.custom(layoutSize: .init(widthDimension: .fractionalWidth(1.5), heightDimension: .fractionalHeight(2))) { NSCollectionLayoutEnvironment in
+//            
+//        }
+        
+        
         let section = NSCollectionLayoutSection(group: group)
         section.orthogonalScrollingBehavior = .continuous
         section.contentInsets.leading = 16
